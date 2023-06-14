8dd374cc8ec29f9a0354092163bdd19e7832d879
TomaszPosluszny <tomkposluszny@gmail.com>
2023-06-14 20:44:52 +0200

    Add new file mathots.js and add two task witch method charAt()

---
+++ mathots.js
@@ -0,0 +1,19 @@
+// 1. charAt()
+
+// Task 1: Write a function that takes a string as an argument and returns the first letter of that string.
+
+function getFirstLetter(text) {
+	return text.charAt(0);
+}
+
+const text = 'Sample text';
+console.log(getFirstLetter(text));
+
+// Task 2: Write a function that takes a string as an argument and returns the last letter of that string.
+
+function getLastLetter(text) {
+    return textOne.charAt(text.length - 1);
+  }
+  
+  const textOne = "Another task";
+  console.log(getLastLetter(textOne)); 
