18c4aedfe14dce72663af7eab0eff950bf7fd477
TomaszPosluszny <tomkposluszny@gmail.com>
2023-06-15 18:54:36 +0200

    Add two task witch method toUpperCase()

--- mathots.js
+++ mathots.js
@@ -12,8 +12,26 @@ console.log(getFirstLetter(text));
 // Task 2: Write a function that takes a string as an argument and returns the last letter of that string.
 
 function getLastLetter(text) {
-    return textOne.charAt(text.length - 1);
-  }
-  
-  const textOne = "Another task";
-  console.log(getLastLetter(textOne)); 
+	return textOne.charAt(text.length - 1);
+}
+
+const textOne = 'Another task';
+console.log(getLastLetter(textOne));
+
+// 2. toUpperCase()
+
+//  Task 1: Write a function that takes two strings as arguments and returns a new string where both strings are converted to uppercase. Use the toUpperCase() method.
+function mergeAndConvertToUppercase(str1, str2) {
+	const mergedString = str1.concat(str2);
+	const uppercaseString = mergedString.toUpperCase();
+	return uppercaseString;
+}
+
+const result = mergeAndConvertToUppercase('Hello', 'World');
+console.log(result);
+
+//Task2: Write a program that prompts the user for a string, converts all the letters to uppercase using the toUpperCase() method, and then displays the modified string on the screen
+
+const input = prompt('Enter a string:');
+const uppercaseString = input.toUpperCase();
+console.log('Modified string:', uppercaseString);
