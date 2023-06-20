bcf4cad224626dc338a009f3fcab2ea26c1a6931
TomaszPosluszny <tomkposluszny@gmail.com>
2023-06-20 20:02:10 +0200

    Add two task witch method join()

--- mathots.js
+++ mathots.js
@@ -37,49 +37,68 @@ const uppercaseString = input.toUpperCase();
 
 console.log('Modified string:', uppercaseString);
 
-
 // 3 toLowerCase()
 
 // Task 1:  Write a function that takes two strings as arguments and returns a new string where both strings are converted to lowercase. Use the toLowerCase() method.
 
-function mergeAndConvertToLowerCase(a,b){
-const mergeString = a.concat(b)
-const lowercaseString = mergeString.toLowerCase()
-return lowercaseString
+function mergeAndConvertToLowerCase(a, b) {
+	const mergeString = a.concat(b);
+	const lowercaseString = mergeString.toLowerCase();
+	return lowercaseString;
 }
 
-const resultLower = mergeAndConvertToLowerCase("Hello", "World")
+const resultLower = mergeAndConvertToLowerCase('Hello', 'World');
 console.log(resultLower);
-
 
 // Task 2: Write a program that prompts the user for a string, converts all the letters to lowercase using the toLowerCase() method, and then displays the modified string on the screen.
 
-const inputLover = prompt("Enter a string:");
+const inputLover = prompt('Enter a string:');
 var lowercaseString = inputLover.toLowerCase();
-console.log("Modified string:", lowercaseString);
+console.log('Modified string:', lowercaseString);
 
 // split()
 
 //  Task 1: Counting words in a sentence The task is to determine the number of words in a given sentence. We can use the split() method to split the sentence into an array of words, and then count the number of elements in that array.
 
 function countWords(sentence) {
-    const words = sentence.split(" ");
-    return words.length;
-  }
-  
-  const sentence = "This is an example sentence.";
-  const wordCount = countWords(sentence);
-  console.log("Word count: " + wordCount);
+	const words = sentence.split(' ');
+	return words.length;
+}
 
-  // Task 2: Reversing a string.The task is to reverse a given string. We can use the split() method to convert the string into an array, then use the reverse() method to reverse the order of elements in the array. Finally, we use the join() method to concatenate the reversed characters back into a single string.
+const sentence = 'This is an example sentence.';
+const wordCount = countWords(sentence);
+console.log('Word count: ' + wordCount);
 
-  function reverseString(inputString) {
-    const charArray = inputString.split("");
-    const reversedArray = charArray.reverse();
-    const reversedString = reversedArray.join("");
-    return reversedString;
-  }
-  
-  const stringToReverse = "Hello, World!";
-  const reversedString = reverseString(stringToReverse);
-  console.log("Reversed string: " + reversedString);
+// Task 2: Reversing a string.The task is to reverse a given string. We can use the split() method to convert the string into an array, then use the reverse() method to reverse the order of elements in the array. Finally, we use the join() method to concatenate the reversed characters back into a single string.
+
+function reverseString(inputString) {
+	const charArray = inputString.split('');
+	const reversedArray = charArray.reverse();
+	const reversedString = reversedArray.join('');
+	return reversedString;
+}
+
+const stringToReverse = 'Hello, World!';
+const reversedString = reverseString(stringToReverse);
+console.log('Reversed string: ' + reversedString);
+
+// join()
+
+// Task 1: Joining array elements into a string. The task is to concatenate the elements of an array into a single string, separating them with a specified separator. In this case, we can use the join() method to combine the array elements into a string, using the separator as the method's argument.
+
+const fruits = ['apple', 'banana', 'orange'];
+const joinedString = fruits.join(', ');
+console.log('Joined string: ' + joinedString);
+
+//Task 2: Generating time format. The task is to generate a time format in the form of hour:minute based on two numbers representing the hour and minute. We can use the join() method to concatenate these two numbers into a string, separating them with a colon.
+
+function formatTime(hour, minute) {
+	var timeArray = [hour, minute];
+	var formattedTime = timeArray.join(':');
+	return formattedTime;
+}
+
+var hour = 9;
+var minute = 30;
+var formattedTimeString = formatTime(hour, minute);
+console.log('Formatted time: ' + formattedTimeString);
